d83e534c307fa19cdfa0d58c39d977cb4ef7dba6
mmxxff-0222 <2410439857@qq.com>
2022-12-25 22:08:53 +0800

    修改个人资料（昵称、密码、手机、头像、性别、生日）

---
+++ frontend/app/src/main/java/com/cdtde/chongdetang/controller/my/newNameActivity.java
@@ -0,0 +1,71 @@
+package com.cdtde.chongdetang.controller.my;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cdtde.chongdetang.R;
+import com.cdtde.chongdetang.databinding.ActicityNewnameBinding;
+
+public class newNameActivity extends AppCompatActivity {
+    private ActicityNewnameBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActicityNewnameBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        initViews();
+        setListener();//设置监听事件
+    }
+
+    private void setListener() {
+        binding.newNameSave.setOnClickListener(v -> {
+            String content=binding.newNameContent.getText().toString();//用户输入内容
+            Bundle bundle=new Bundle();
+            bundle.putString("new_name",content);
+            Intent intent=getIntent();
+            intent.putExtras(bundle);
+            intent.setClass(newNameActivity.this, personInfoActivity.class);
+//                startActivity(intent);
+            setResult(1,intent);
+            finish();
+
+
+//            Toast.makeText(this, content, Toast.LENGTH_SHORT).show();
+//            finish();
+        });
+    }
+    public static void actionStart(Context context) {
+        Intent intent = new Intent(context, newNameActivity.class);
+        context.startActivity(intent);
+    }
+
+    private void initViews() {
+        setSupportActionBar(binding.settingsToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.arrow_left);
+        }
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+//        input=findViewById(R.id.feedback_content);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return true;
+    }
+}
---
+++ frontend/app/src/main/java/com/cdtde/chongdetang/controller/my/newPhoneActivity.java
@@ -0,0 +1,86 @@
+package com.cdtde.chongdetang.controller.my;
+
+        import android.content.Context;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Toast;
+
+        import androidx.annotation.NonNull;
+        import androidx.appcompat.app.ActionBar;
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import com.cdtde.chongdetang.R;
+        import com.cdtde.chongdetang.controller.SettingActivity;
+        import com.cdtde.chongdetang.databinding.ActicityNewnameBinding;
+        import com.cdtde.chongdetang.databinding.ActivityNewPhoneBinding;
+
+public class newPhoneActivity extends AppCompatActivity {
+    private ActivityNewPhoneBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityNewPhoneBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        initViews();
+        setListener();//设置监听事件
+    }
+
+    private void setListener() {
+        //按钮
+        binding.newPhoneCommit.setOnClickListener(v -> {
+            String phoneCode=binding.newPhoneEnterCodeEv.getText().toString();//用户输入内容
+            if (phoneCode.isEmpty()){
+                Toast.makeText(this, "验证码不能为空", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                //检验验证码
+                Toast.makeText(this, "验证码："+phoneCode, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+        });
+        //获取验证码
+        binding.newPhoneGetCodeTv.setOnClickListener(v -> {
+            String phoneNumber=binding.newPhoneEnterPhoneEv.getText().toString();//新手机号
+            if (phoneNumber.isEmpty()) {
+                Toast.makeText(this, "手机号不能为空", Toast.LENGTH_SHORT).show();
+            }
+            else if (phoneNumber.length()!=11  || !phoneNumber.matches("[0-9]+")){
+                Toast.makeText(this, "请输入正确手机号", Toast.LENGTH_SHORT).show();
+            }
+            else
+                //发送验证码
+                Toast.makeText(this, "向"+phoneNumber+"发送验证码", Toast.LENGTH_SHORT).show();
+
+        });
+    }
+    public static void actionStart(Context context) {
+        Intent intent = new Intent(context, newPhoneActivity.class);
+        context.startActivity(intent);
+    }
+
+    private void initViews() {
+        setSupportActionBar(binding.settingsToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.arrow_left);
+        }
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+//        input=findViewById(R.id.feedback_content);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return true;
+    }
+}
