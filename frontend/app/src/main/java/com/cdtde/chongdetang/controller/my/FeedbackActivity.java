56ca5c315147064e0f1deceb3e0b0cd211cf0894
mmxxff-0222 <2410439857@qq.com>
2022-12-25 21:50:04 +0800

    软件意见反馈

---
+++ frontend/app/src/main/java/com/cdtde/chongdetang/controller/my/FeedbackActivity.java
@@ -0,0 +1,62 @@
+package com.cdtde.chongdetang.controller.my;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cdtde.chongdetang.R;
+import com.cdtde.chongdetang.databinding.ActivityFeedbackBinding;
+
+public class FeedbackActivity extends AppCompatActivity {
+    private ActivityFeedbackBinding binding;
+//    private EditText input;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityFeedbackBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        initViews();
+        setListener();//设置监听事件
+    }
+
+    private void setListener() {
+        binding.feedbackCommit.setOnClickListener(v -> {
+            String content=binding.feedbackContent.getText().toString();//用户输入内容
+            Toast.makeText(this, content, Toast.LENGTH_SHORT).show();
+            finish();
+        });
+    }
+    public static void actionStart(Context context) {
+        Intent intent = new Intent(context, FeedbackActivity.class);
+        context.startActivity(intent);
+    }
+
+    private void initViews() {
+        setSupportActionBar(binding.settingsToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.arrow_left);
+        }
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+//        input=findViewById(R.id.feedback_content);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return true;
+    }
+}
