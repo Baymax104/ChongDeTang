039d4f926762c9a75ab4e3bfa17a1cdceebd66cd
mmxxff-0222 <2410439857@qq.com>
2022-12-25 22:07:41 +0800

    "我的"收货地址

---
+++ frontend/app/src/main/java/com/cdtde/chongdetang/controller/my/adapters/ListInScroll.java
@@ -0,0 +1,26 @@
+package com.cdtde.chongdetang.controller.my.adapters;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class ListInScroll extends ListView {
+    public ListInScroll(Context context) {
+        super(context);
+    }
+
+    public ListInScroll(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ListInScroll(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int measuredHeight = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);//只写了这一句就搞定了
+        super.onMeasure(widthMeasureSpec, measuredHeight);//这里需要将第二个参数改为我们测量好的measureHeight
+    }
+}
+
+
