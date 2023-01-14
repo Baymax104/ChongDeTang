66046a6414408e7d606829c915cc83dc8465b256
mmxxff-0222 <2410439857@qq.com>
2023-01-15 02:15:31 +0800

    完成主页"崇德缘起"子页面。（使用AS自带的tab activity模版）

---
+++ frontend/app/src/main/java/com/cdtde/chongdetang/view/index/OriginWebFragment.java
@@ -0,0 +1,67 @@
+package com.cdtde.chongdetang.view.index;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.cdtde.chongdetang.databinding.FragmentOriginBinding;
+import com.cdtde.chongdetang.util.WebViewUtil;
+import com.cdtde.chongdetang.viewModel.OriginViewModel;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class OriginWebFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private OriginViewModel vm;
+    private FragmentOriginBinding binding;
+
+    public static OriginWebFragment newInstance(int index) {
+        OriginWebFragment fragment = new OriginWebFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        vm = new ViewModelProvider(this).get(OriginViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        vm.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        binding = FragmentOriginBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        String webUrl = vm.getUrl();   //获取视图的网页地址
+        WebViewUtil.configure(binding.originWebPage, true);
+        binding.originWebPage.loadUrl(webUrl);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
